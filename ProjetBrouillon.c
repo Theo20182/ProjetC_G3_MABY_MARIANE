9f1a25709edaf74edd390e853bba72c8cedc53b0
m-ariane <48915961+m-ariane@users.noreply.github.com>
2019-03-25 14:45:36 +0100

    Create ProjetBrouillon.c

---
+++ ProjetBrouillon.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+#define lambda 0.7 //devient malade, donc 1-lambda prob rester sain
+#define gamma 0.2 //devient immunisé, donc 0.8 prob décès
+//on considère pour le moment que l'état "malade" dure qu'un jour t
+
+typedef struct
+{ 
+  int id; //on les identifie/différencie par des nombres
+  enum  etat;
+  int voisins[];
+} individu
+  
+
