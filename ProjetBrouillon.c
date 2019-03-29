d565f58ebe61c5dccad6801f26539fca92d04192
m-ariane <48915961+m-ariane@users.noreply.github.com>
2019-03-29 09:39:24 +0100

    Update ProjetBrouillon.c

--- ProjetBrouillon.c
+++ ProjetBrouillon.c
@@ -4,6 +4,8 @@
 
 
 /*************** DEFINE ***************/
+#define VOISIN_MAX  7
+#define LOVE_MAX    10
 #define lambda 0.7 //devient malade, donc 1-lambda prob rester sain
 #define gamma 0.2 //devient immunisé, donc 0.8 prob décès
 //on considère pour le moment que l'état "malade" dure qu'un jour t
@@ -11,6 +13,7 @@
 
 
 /*************** PROTOTYPES ***************/
+void writeFile(int L);
 int calcul_nb_arcs;
 void creation_graphe(graphe* G)
 void first_sick(void);
@@ -38,6 +41,36 @@ enum (SAIN, MALADE, IMMUNISE, MORT);
 
 
 /*************** FONCTIONS ***************/
+
+void writeFile(int L) {
+    int v, l;   //L'id du voisin, le degré de love
+    int j;      //Le nombre de voisin   
+    srand(time(NULL));
+    
+    char name_file[20];
+    FILE *fp;
+    
+    printf("Entrez le nom du fichier créer: ");
+    scanf("%s", name_file);
+    fp = fopen(name_file, "w");
+    fprintf(fp, "%d\n", L);
+    
+    for(int i = 1; i <= L; i++) {  //On itère sur l'id de l'individu
+        j = rand()%VOISIN_MAX; //On choisit son nombre de voisin
+        for(int k = 0; k <= j; k++){    //On itère sur l'id de voisin
+            v = rand()%L + 1;       //On choisit l'id du voisin
+            if(v != i){                     //On s'assure que                                           l'individu n'est pas son                                    propre voisin                           
+                l = rand()%LOVE_MAX + 1; //On choisit le degré de love
+                fprintf(fp, "%d %d %d\n", i, v, l); //On écrit
+            }
+        }
+    }
+    fclose(fp);
+}
+//Probleme: Eviter le cas pathologique de doublons et determiner le nopmbre d'arc dans le graphe
+
+
+
 int calcul_nb_arcs
 {
 	int nb_arcs;
@@ -108,5 +141,12 @@ void first_sick(void)
 {
   
   
+}
+
+
+
+int main(void) {
+    int L = 10;
+    writeFile(L);
 }
 
